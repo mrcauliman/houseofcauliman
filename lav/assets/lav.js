e60d0b03e73c224fb5244fe97e4ac659f7dd5ee4
mrcauliman <mrcauliman@gmail.com>
2026-04-20 22:20:48 -0700

    lav: add initial site shell and internal pages

---
+++ lav/assets/lav.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const yearNodes = document.querySelectorAll("[data-current-year]");
+  const year = new Date().getFullYear();
+  yearNodes.forEach((node) => {
+    node.textContent = String(year);
+  });
+});
